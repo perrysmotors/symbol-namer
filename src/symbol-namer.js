bf78606156c4ead6217655d286de2b4d2b951c21
Giles Perry <giles@gilesperry.info>
2020-03-05 15:51:24 +0000

    Prettier

--- src/symbol-namer.js
+++ src/symbol-namer.js
@@ -1,34 +1,40 @@
 var UI = require("sketch/ui"),
-  DOM = require("sketch/dom");
+    DOM = require("sketch/dom")
 
 export function onRename(context) {
-  var document = DOM.getSelectedDocument();
+    var document = DOM.getSelectedDocument()
 
-  var selectedLayers = document.selectedLayers.map(layer => layer).filter(layer => layer.type === 'SymbolInstance');
-  var selectedCount = selectedLayers.length;
-  
-  if (selectedCount === 0) {
-    UI.message('Select one or more symbols');
-  } else {
-    let renamedCount = 0;
-    selectedLayers.forEach(layer => {
-      var overrides = layer.overrides.filter(override => override.property === 'stringValue' && override.sketchObject.isEditable());
-      var overrideCount = overrides.length;
+    var selectedLayers = document.selectedLayers
+        .map(layer => layer)
+        .filter(layer => layer.type === "SymbolInstance")
+    var selectedCount = selectedLayers.length
 
-      if (overrideCount > 0) {
-        layer.name = overrides[0].value;
-        renamedCount++
-      }
-    });
-    switch (renamedCount) {
-      case 0:
-      UI.message("None of the selected symbols have text overrides");
-      break;
-      case 1:
-        UI.message("1 symbol renamed");
-        break;
-      default:
-        UI.message(`${renamedCount} symbols renamed`);
+    if (selectedCount === 0) {
+        UI.message("Select one or more symbols")
+    } else {
+        let renamedCount = 0
+        selectedLayers.forEach(layer => {
+            var overrides = layer.overrides.filter(
+                override =>
+                    override.property === "stringValue" &&
+                    override.sketchObject.isEditable()
+            )
+            var overrideCount = overrides.length
+
+            if (overrideCount > 0) {
+                layer.name = overrides[0].value
+                renamedCount++
+            }
+        })
+        switch (renamedCount) {
+            case 0:
+                UI.message("None of the selected symbols have text overrides")
+                break
+            case 1:
+                UI.message("1 symbol renamed")
+                break
+            default:
+                UI.message(`${renamedCount} symbols renamed`)
+        }
     }
-  }
 }
